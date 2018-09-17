762569b2294ceef7e6c4a98ed26458ac11efbfd4
Noah Huppert <contact@noahh.io>
2018-09-17 11:55:02 -0400

    wrote challenge insert method

--- models/challenge.go
+++ models/challenge.go
@@ -2,18 +2,20 @@ package models
 
 import (
 	"time"
+
+	"github.com/jmoiron/sqlx"
 )
 
 const (
-	// StatusAnswering indicate that a caller is currently answering
+	// ChallengeStatusAnswering indicate that a caller is currently answering
 	// a challenge
-	StatusAnswering string = "ANSWERING"
+	ChallengeStatusAnswering string = "ANSWERING"
 
-	// StatusFailed indicates that a caller failed a challenge
-	StatusFailed string = "FAILED"
+	// ChallengeStatusFailed indicates that a caller failed a challenge
+	ChallengeStatusFailed string = "FAILED"
 
-	// StatusPassed indicates a caller succeeded in answering a challenge
-	StatusPassed string = "PASSED"
+	// ChallengeStatusPassed indicates a caller succeeded in answering a challenge
+	ChallengeStatusPassed string = "PASSED"
 )
 
 // Challenge holds information about a question asked to a caller. Used to
@@ -40,4 +42,32 @@ type Challenge struct {
 	// Status indicates if the caller is currently answering the question,
 	// failed to answer the question, or succeeded to answer the question
 	Status string
+}
+
+// Insert adds a challenge row to the challenges database. The ID field is
+// updated with the inserted row's ID.
+func (c *Challenge) Insert(db *sqlx.DB) error {
+	// Start transaction
+	tx, err := db.Beginx()
+	if err != nil {
+		return fmt.Errorf("error starting transaction: %s", err.Error())
+	}
+
+	// Insert
+	err = tx.QueryRowx("INSERT INTO challenges (phone_call_id, date_asked, "+
+		"operand_a, operand_b, solution, status) VALUES ($1, $2, $3, $4, $5, "+
+		"$6)", c.PhoneCallID, c.DateAsked, c.OperandA, c.OperandB, c.Solution,
+		c.Status).StructScan(c)
+
+	if err != nil {
+		return fmt.Errorf("error executing insert statement: %s", err.Error())
+	}
+
+	// Commit transaction
+	err = tx.Commit()
+	if err != nil {
+		return fmt.Errorf("error committing transaction: %s", err.Error())
+	}
+
+	return nil
 }
