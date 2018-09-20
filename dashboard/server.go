e89a0a32c3e443a77939cc88cf8d2733c6e29d71
Noah Huppert <contact@noahh.io>
2018-09-20 16:50:46 -0400

    added method to setup dashboard server

---
+++ dashboard/server.go
@@ -0,0 +1,24 @@
+package dashboard
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/Noah-Huppert/human-call-filter/config"
+
+	"github.com/gorilla/mux"
+)
+
+// NewServer creates a new http.Server which serves an internal API and
+// dashboard page
+func NewServer(cfg *config.Config) http.Server {
+	router := mux.NewRouter()
+
+	// File server
+	router.Handle("/", http.FileServer(http.Dir("./static")))
+
+	return http.Server{
+		Addr:    fmt.Sprintf(":%s", cfg.DashboardHTTPPort),
+		Handler: router,
+	}
+}
