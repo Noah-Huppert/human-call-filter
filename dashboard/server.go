e120996388753ae24936a8a51203e5c234e6cd7f
Noah Huppert <contact@noahh.io>
2018-09-20 20:45:04 -0400

    fixed dashboard http handler to serve content

--- dashboard/server.go
+++ dashboard/server.go
@@ -5,20 +5,19 @@ import (
 	"net/http"
 
 	"github.com/Noah-Huppert/human-call-filter/config"
-
-	"github.com/gorilla/mux"
+	//"github.com/gorilla/mux"
 )
 
 // NewServer creates a new http.Server which serves an internal API and
 // dashboard page
 func NewServer(cfg *config.Config) http.Server {
-	router := mux.NewRouter()
+	//router := mux.NewRouter()
 
 	// File server
-	router.Handle("/", http.FileServer(http.Dir("./static")))
+	//router.Handle("/", http.FileServer(http.Dir("static")))
 
 	return http.Server{
 		Addr:    fmt.Sprintf(":%s", cfg.DashboardHTTPPort),
-		Handler: router,
+		Handler: http.FileServer(http.Dir("static")),
 	}
 }
