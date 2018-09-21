38aed15ea001b8eabb5f0d3cf99e53d774efc129
Noah Huppert <contact@noahh.io>
2018-09-21 02:13:05 -0400

    dashboard: added table row selection, added calls page

--- static/js/app.js
+++ static/js/app.js
@@ -13,7 +13,6 @@ function toggleNavbarMenu() {
 }
 
 function closeNavbarMenu() {
-	console.log("close");
 	document.getElementById("nav-menu").classList.remove("is-active");
 }
 
@@ -39,6 +38,11 @@ Vue.component("navbar-menu", {
 					<li v-on:click="closeNavbarMenu">
 						<router-link to="/numbers">
 							Numbers
+						</router-link>
+					</li>
+					<li v-on:click="closeNavbarMenu">
+						<router-link to="/calls">
+							Calls
 						</router-link>
 					</li>
 				</ul>
@@ -67,17 +71,16 @@ const phoneNumbersPage = Vue.component("phone-numbers-page", {
 				</tr>
 			</thead>
 			<tbody>
-				<tr v-for="number in phoneNumbers">
-					<td>{{ number.ID }}</td>
-					<td>{{ number.Number }}</td>
-					<td>{{ number.Name }}</td>
-					<td>{{ number.State }}</td>
-					<td>{{ number.City }}</td>
-					<td>{{ number.ZipCode }}</td>
-				</tr>
+				<phone-number-row v-for="number in phoneNumbers"
+					v-bind:number="number"
+					v-bind:selected-id="id">
+				</phone-number-row>
 			</tbody>
 		</table>
 	</div>`,
+	props: {
+		id: undefined
+	},
 	data: function() {
 		return {
 			phoneNumbers: this.phoneNumbers
@@ -94,11 +97,127 @@ const phoneNumbersPage = Vue.component("phone-numbers-page", {
 	}
 });
 
+Vue.component("phone-number-row", {
+	template: `<tr v-bind:class="{ selected: isSelected }">
+		<td>{{ number.ID }}</td>
+		<td>{{ number.Number }}</td>
+		<td>{{ number.Name }}</td>
+		<td>{{ number.State }}</td>
+		<td>{{ number.City }}</td>
+		<td>{{ number.ZipCode }}</td>
+	</tr>`,
+	props: ["number", "selected-id"],
+	data: function() {
+		return {
+			isSelected: this.getIsSelected()
+		};
+	},
+	watch: {
+		selectedId: function() {
+			this.isSelected = this.getIsSelected();
+		}
+	},
+	methods: {
+		getIsSelected: function() {
+			return this.number.ID == this.selectedId;
+		}
+	}
+});
+
+const phoneCallsPage = Vue.component("phone-calls-page", {
+	template: `<div class="container">
+		<h1 class="title">Phone Calls</h1>
+
+		<table class="table">
+			<thead>
+				<tr>
+					<th>ID</th>
+					<th>Phone Number ID</th>
+					<th>Twilio Call ID</th>
+					<th>Date Received</th>
+				</tr>
+			</thead>
+			<tbody>
+				<phone-call-row v-for="call in phoneCalls"
+					v-bind:call="call"
+					v-bind:selected-id="id">
+				</phone-call-row>
+			</tbody>
+		</table>
+	</div>`,
+	props: {
+		id: undefined
+	},
+	data: function() {
+		return {
+			phoneCalls: this.phoneCalls
+		};
+	},
+	created: function() {
+		this.phoneCalls = [];
+		var self = this;
+
+		makeAPIRequest("/api/phone_calls", "GET")
+			.then(function(resp) {
+				self.phoneCalls = resp.phone_calls;
+			});
+	}
+});
+
+Vue.component("phone-call-row", {
+	template: `<tr v-bind:class="{ selected: isSelected }">
+		<td>{{ call.ID }}</td>
+		<td>
+			<router-link v-bind:to="'/numbers?id=' + call.PhoneNumberID">
+				{{ call.PhoneNumberID }}
+			</router-link>
+		</td>
+		<td>{{ call.TwilioCallID }}</td>
+		<td>{{ call.DateReceived }}</td>
+	</tr>`,
+	props: ["call", "selected-id"],
+	data: function() {
+		return {
+			isSelected: this.getIsSelected()
+		};
+	},
+	watch: {
+		selectedId: function() {
+			this.isSelected = this.getIsSelected();
+		}
+	},
+	methods: {
+		getIsSelected: function() {
+			return this.call.ID == this.selectedId;
+		}
+	}
+});
+
 /* Router */
 const router = new VueRouter({
 	routes: [
-		{ path: "/", redirect: "/numbers" },
-		{ path: "/numbers", component: phoneNumbersPage }
+		{
+			path: "/",
+			redirect: "/numbers"
+		},
+		{
+			path: "/numbers",
+			component: phoneNumbersPage,
+			props: function(route) {
+				return {
+					id: route.query.id
+				};
+			}
+		},
+		{
+			path: "/calls",
+			component: phoneCallsPage,
+			props: function(route) {
+				return {
+					id: route.query.id
+				};
+			}
+		}
 	]
 });
 
